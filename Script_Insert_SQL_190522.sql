dcc3d413f9533d842ba3c9a1bdb8e33f24659b41
Олег Матшин <89175955650@mail.ru>
2022-05-19 20:36:32 +0300

    INSERT SQL singer_list and style_singer tables txt

--- Script_Insert_SQL_190522.sql
+++ Script_Insert_SQL_190522.sql
@@ -28,6 +28,107 @@ INSERT INTO style_list(style_name)
 INSERT INTO style_list(style_name) 
 	VALUES('techno');
 
+INSERT INTO singer_list(singer_name) 
+	VALUES('Vladimir Petrov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Oleg Ivanov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Ivan Bobrov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Sergey Dmitriev');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Nicolay Zlobin');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Vlad Trofimov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Marat Ibragimov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Andrey Sysoev');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Timur Rebrov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Eduard Blohin');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Boris Voronov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Anton Kozlov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Foma Nikandrov');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Vasilisa Premudraya');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Elena Prekrasnaya');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Primadonna');
+
+INSERT INTO singer_list(singer_name) 
+	VALUES('Trololo');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Vladimir Petrov', 'rock');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Oleg Ivanov', 'pop');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Ivan Bobrov', 'jazz');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Sergey Dmitriev', 'hip-hop');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Nicolay Zlobin', 'classic');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Vlad Trofimov', 'techno');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Marat Ibragimov', 'alternative');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Andrey Sysoev', 'electronic');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Timur Rebrov', 'soul');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Eduard Blohin', 'country');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Boris Voronov', 'pop');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Anton Kozlov', 'rock');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Foma Nikandrov', 'jazz');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Vasilisa Premudraya', 'pop');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Elena Prekrasnaya', 'classic');
+
+INSERT INTO style_singer(singer_id, style_id) 
+	VALUES('Primadonna', 'rock');
+
+INSERT INTO style_singer(singer_id, style_id)
+	VALUES('Trololo', 'hip-hop');
 
 
 
