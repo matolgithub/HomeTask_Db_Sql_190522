3f9c1b8c6c0f0d10a0b8d84e008940e0c675a782
Олег Матшин <89175955650@mail.ru>
2022-05-19 19:41:19 +0300

    INSERT SQL style_list table txt

---
+++ Script_Insert_SQL_190522.sql
@@ -0,0 +1,35 @@
+INSERT INTO style_list(style_name) 
+	VALUES('rock');
+
+INSERT INTO style_list(style_name) 
+	VALUES('pop');
+
+INSERT INTO style_list(style_name) 
+	VALUES('jazz');
+
+INSERT INTO style_list(style_name) 
+	VALUES('hip-hop');
+
+INSERT INTO style_list(style_name) 
+	VALUES('country');
+
+INSERT INTO style_list(style_name) 
+	VALUES('electronic');
+
+INSERT INTO style_list(style_name) 
+	VALUES('soul');
+
+INSERT INTO style_list(style_name) 
+	VALUES('alternative');
+
+INSERT INTO style_list(style_name) 
+	VALUES('classic');
+
+INSERT INTO style_list(style_name) 
+	VALUES('techno');
+
+
+
+
+
+	
